7438f7f8506c4bcb355dc7200ac8ba5383712d24
wa1tnr <wa1tnr@users.noreply.github.com>
2019-12-23 19:57:59 +0000

    latest for STM32F405 and Trinket M0

--- m0_terminal_uart.ino
+++ m0_terminal_uart.ino
@@ -1,3 +1,9 @@
+// Mon Dec 23 19:55:26 UTC 2019
+
+// for STM32F405 project - this sketch runs on Trinket M0,
+// as part of a two-targt project (eForth).
+
+// old:
 // Wed Jan 31 00:39:34 UTC 2018
 // 4737-a0e-03a-
 
