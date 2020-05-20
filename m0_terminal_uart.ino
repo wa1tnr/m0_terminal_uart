8cd74704e427c7aae1608f7ce771c4670cb4bcae
wa1tnr <wa1tnr@users.noreply.github.com>
2020-05-20 15:58:53 +0000

    Functioning sketch.
    This sketch works as-is, STM32F407 Discovery,
    talking to an ItsyBitsy M4 on its TX/RX pair.
    
    Using Ting's eForth, unmodified.
    
    	modified:   m0_terminal_uart.ino
    
    On branch stm407dsc-aa-

--- m0_terminal_uart.ino
+++ m0_terminal_uart.ino
@@ -1,3 +1,6 @@
+// Wed May 20 15:57:58 UTC 2020
+// status: works with current hardware setup (ItsyBitsyM4, STM32F407 Discovery)
+
 // Mon Dec 23 19:55:26 UTC 2019
 
 // for STM32F405 project - this sketch runs on Trinket M0,
